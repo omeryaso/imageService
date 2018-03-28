89820a189b06fd4fb99b0df6b947c5d07a3420ab
Shlomy <Shlomy@LAPTOP-OISRR22I>
2018-03-28 15:56:11 +0300

    Initial commit

---
+++ MyNewService/ImageService.Designer.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ImageService
+{
+    partial class ImageService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // ImageService
+            // 
+            this.ServiceName = "ImageService";
+
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+        #endregion
+
+
+    }
+}
