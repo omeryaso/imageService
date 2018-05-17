47ec51563444ad913bbfcbadb988933aa7e2aa07
Shlomy <Shlomy@LAPTOP-OISRR22I>
2018-05-17 11:09:14 +0300

    Created the Model, ViewModel classes

---
+++ ImageServiceGUI/Model/LogsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI.Model
+{
+    class LogsModel : ILogsModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ImageServiceGUI/ViewModel/LogsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI.ViewModel
+{
+    class LogsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
